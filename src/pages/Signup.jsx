ff1e6f2bb7eb08b229887d3ee9a9c4cbf07bcc85
jyhan14 <jyhan0114@gmail.com>
2023-06-11 22:09:51 +0900

    regex 수정

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -26,7 +26,7 @@ const Signup = () => {
   const [isButtonDisabled, setIsButtonDisabled] = useState(true);
 
   const regEmail = /@.*/;
-  const regPassword = /^.{8,}$/;
+  const regPassword = /^.{7,}$/;
 
   const onChangeHandler = (e) => {
     const { name, value } = e.target;
